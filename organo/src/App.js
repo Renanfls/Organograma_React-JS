796e2602df120e799124b775d5e260b985ff7e75
Renanfls <contato.renanfabricio@gmail.com>
2022-09-16 13:59:26 -0300

    13:58 16/09/2022

--- organo/src/App.js
+++ organo/src/App.js
@@ -52,14 +52,14 @@ function App() {
 
   return (
     <div className="App">
-      <Banner /> 
+      <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
 
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria} 
+      {times.map(time => <Time
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria}
+        corSecundaria={time.corSecundaria}
         colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
 
